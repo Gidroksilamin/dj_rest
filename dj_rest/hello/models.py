5592490c53c30f01d5a8f262cc9f3eda7652cabc
gid <gidroksilamin@gmail.com>
2023-03-14 21:42:48 +0500

    Organization and CustomUser classes added

--- dj_rest/hello/models.py
+++ dj_rest/hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=20)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    organizations = models.ManyToManyField(Organization)
